393ee499b69b0113b1126b9a96dba32c40714292
nguyentrongtaii <nguyentai097162@gmail.com>
2024-04-10 23:48:28 +0700

    done sub-menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,22 +11,22 @@ const $$ = document.querySelectorAll.bind(document);
  * </script>
  */
 function load(selector, path) {
-      const cached = localStorage.getItem(path);
-      if (cached) {
-            $(selector).innerHTML = cached;
-      }
+  const cached = localStorage.getItem(path);
+  if (cached) {
+    $(selector).innerHTML = cached;
+  }
 
-      fetch(path)
-            .then((res) => res.text())
-            .then((html) => {
-                  if (html !== cached) {
-                        $(selector).innerHTML = html;
-                        localStorage.setItem(path, html);
-                  }
-            })
-            .finally(() => {
-                  window.dispatchEvent(new Event("template-loaded"));
-            });
+  fetch(path)
+    .then((res) => res.text())
+    .then((html) => {
+      if (html !== cached) {
+        $(selector).innerHTML = html;
+        localStorage.setItem(path, html);
+      }
+    })
+    .finally(() => {
+      window.dispatchEvent(new Event("template-loaded"));
+    });
 }
 
 /**
@@ -34,21 +34,21 @@ function load(selector, path) {
  * có bị ẩn bởi display: none không
  */
 function isHidden(element) {
-      if (!element) return true;
+  if (!element) return true;
 
-      if (window.getComputedStyle(element).display === "none") {
-            return true;
-      }
+  if (window.getComputedStyle(element).display === "none") {
+    return true;
+  }
 
-      let parent = element.parentElement;
-      while (parent) {
-            if (window.getComputedStyle(parent).display === "none") {
-                  return true;
-            }
-            parent = parent.parentElement;
-      }
+  let parent = element.parentElement;
+  while (parent) {
+    if (window.getComputedStyle(parent).display === "none") {
+      return true;
+    }
+    parent = parent.parentElement;
+  }
 
-      return false;
+  return false;
 }
 
 /**
@@ -56,13 +56,13 @@ function isHidden(element) {
  * sau một khoảng thời gian mới được thực thi
  */
 function debounce(func, timeout = 300) {
-      let timer;
-      return (...args) => {
-            clearTimeout(timer);
-            timer = setTimeout(() => {
-                  func.apply(this, args);
-            }, timeout);
-      };
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
 }
 
 /**
@@ -73,14 +73,14 @@ function debounce(func, timeout = 300) {
  * 2. CSS "left" cho arrow qua biến "--arrow-left-pos"
  */
 const calArrowPos = debounce(() => {
-      if (isHidden($(".js-dropdown-list"))) return;
+  if (isHidden($(".js-dropdown-list"))) return;
 
-      const items = $$(".js-dropdown-list > li");
+  const items = $$(".js-dropdown-list > li");
 
-      items.forEach((item) => {
-            const arrowPos = item.offsetLeft + item.offsetWidth / 2;
-            item.style.setProperty("--arrow-left-pos", `${arrowPos}px`);
-      });
+  items.forEach((item) => {
+    const arrowPos = item.offsetLeft + item.offsetWidth / 2;
+    item.style.setProperty("--arrow-left-pos", `${arrowPos}px`);
+  });
 });
 
 // Tính toán lại vị trí arrow khi resize trình duyệt
@@ -88,3 +88,47 @@ window.addEventListener("resize", calArrowPos);
 
 // Tính toán lại vị trí arrow sau khi tải template
 window.addEventListener("template-loaded", calArrowPos);
+
+/**
+ * Giữ active menu khi hover
+ *
+ * Cách dùng:
+ * 1. Thêm class "js-menu-list" vào thẻ ul menu chính
+ * 2. Thêm class "js-dropdown" vào class "dropdown" hiện tại
+ *  nếu muốn reset lại item active khi ẩn menu
+ */
+window.addEventListener("template-loaded", handleActiveMenu);
+
+function handleActiveMenu() {
+  const dropdowns = $$(".js-dropdown");
+  const menus = $$(".js-menu-list");
+  const activeClass = "top__main--item--active";
+
+  const removeActive = (menu) => {
+    menu.querySelector(`.${activeClass}`)?.classList.remove(activeClass);
+  };
+
+  const init = () => {
+    menus.forEach((menu) => {
+      const items = menu.children;
+      if (!items.length) return;
+
+      removeActive(menu);
+      items[0].classList.add(activeClass);
+
+      Array.from(items).forEach((item) => {
+        item.onmouseenter = () => {
+          if (window.innerWidth <= 991) return;
+          removeActive(menu);
+          item.classList.add(activeClass);
+        };
+      });
+    });
+  };
+
+  init();
+
+  dropdowns.forEach((dropdown) => {
+    dropdown.onmouseleave = () => init();
+  });
+}
